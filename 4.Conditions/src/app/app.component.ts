56124ed6b54b57f9b6b9ccaaff7ec06200e20e90
Egemen Unsur <sabitozbilgic2306@gmail.com>
2024-06-18 15:12:16 +0300

    conditions

--- 4.Conditions/src/app/app.component.ts
+++ 4.Conditions/src/app/app.component.ts
@@ -5,9 +5,36 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+
+    <h1>Angular Conditions</h1>
+    <!-- <p *ngIf="showFirst" style="color: red;">First Yazı</p> eski usül -->
+
+    @if(showFirst){
+      <p style="color: {{showFirst === true ? 'red' : ''}};">First Yazı</p>
+    }
+    
+    @if(showSecond) {
+      <p style="color: green;">Second Yazı</p>
+    }
+
+    <hr>
+
+    <button (click)="show(1)">Birinci Yazıyı Göster</button>
+    <button (click)="show(2)">İkinci Yazıyı Göster</button>
+  `
 })
 export class AppComponent {
-  title = '4.Conditions';
+  showFirst: boolean = false;
+  showSecond: boolean = false;
+
+  show(num: number){
+    if(num === 1){
+      this.showFirst = true;
+      this.showSecond = false;
+    }else{
+      this.showFirst = false;
+      this.showSecond = true;
+    }
+  }
 }
